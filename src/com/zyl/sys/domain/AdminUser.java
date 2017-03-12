8120ec642d9d7a3e1340bea4aa74db256abb12f1
zyl724933134 <724933134@qq.com>
2017-03-12 15:24:54 +0800

    后台用户类

---
+++ src/com/zyl/sys/domain/AdminUser.java
@@ -0,0 +1,23 @@
+package com.zyl.sys.domain;
+
+public class AdminUser {
+	
+	private String name;
+	private int age;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+
+}
